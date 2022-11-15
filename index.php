b899cdca536386b41b73ea88d0a384958b114d62
yusufmahendra <yusufm260901@gmail.com>
2022-11-15 04:49:54 -0800

    perintah tugas 1

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+    echo "Hello Word";
+
+    ?>
+
+<html>
+<head>
+<title>Belajar PHP</title>
+</head>
+<body>
+
+<h1>
+    Selamat Datang <?php echo "Yusuf Mahendra";?>
+</h1>
+
+
+</body>
+</html>
+
