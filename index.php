782a58446fc76535c5256d807c0fa884188791fd
yusufmahendra <yusufm260901@gmail.com>
2022-11-15 06:19:18 -0800

    perintah tugas 5

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-Operator Aritmatika
 $x = 10;
 $y = 20;
-    echo $x + $y ;
-    echo $x - $y ;
-    echo $x * $y ;
-    echo $x / $y ;
-    echo $x % $y ;
-?> 
+
+    echo "$x + $y" ;
+    echo "$x - $y" ;
+    echo "$x * $y" ;
+    echo "$x / $y" ;
+    echo "$x % $y" ;
+    ?> 
