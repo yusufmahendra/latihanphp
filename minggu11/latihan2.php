d5ac5d10c23e55276f9bd57f00b4c675648498f0
yusufmahendra <yusufm260901@gmail.com>
2022-11-23 21:58:33 +0700

    TASK_7 clear

--- minggu11/latihan2.php
+++ minggu11/latihan2.php
@@ -8,9 +8,9 @@
     <h2>Membuat Perulangan Tabel</h2>
     <table border="1" cellpadding="10" cellspacing="0">
         <?php
-        for ($b = 1; $b <= 3; $b++) {
+        for ($b = 1; $b <= 10; $b++) {
             echo "<tr>";
-            for ($k = 1; $k <= 5; $k++) {
+            for ($k = 1; $k <= 8; $k++) {
                 echo "<td> $b,$k </td>";
             }
             echo "</tr>";
