5b4e6d0792032e9c4a1dd5e138bfdc958d48f089
yusufmahendra <yusufm260901@gmail.com>
2022-11-23 20:30:40 +0700

    TASK_2

--- minggu 11/latihan.php
+++
@@ -1,7 +0,0 @@
-<?php;
-// pengulangan
-// for
-
-for($i = 0; $i < 5; i++){
-    echo "Hello Word! <br>";
-}
---
+++ minggu11/latihan.php
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Menggunakan Perulangan For</title>
+</head>
+<body>
+
+<h1>Menggunakan Perulangan For</h1>
+<?php
+
+
+for ($i = 0; $i < 5; $i++){
+    echo "Hai, saya urutan ke- <br>";
+}
+
+
+?>
+</body>
+</html>
